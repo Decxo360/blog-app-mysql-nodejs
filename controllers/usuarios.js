825cb7b85e245f0c2e556c7f8118c3ec1e39d43e
Diego Lundstedt <diegolundstedt99@gmail.com>
2022-09-29 19:11:55 -0300

    metodo crear usuario terminado

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,45 +1,55 @@
 const { query } = require('express');
 const express = require('express');
-const dbConexion = require('../db/config');
 const mysql = require('mysql');
 const bcrypt = require('bcryptjs');
+const { dbConexion, conexion } = require('../db/config');
 
 
-const crearUsuario = async(conecction,req,res)=>{
-
-    const {email, nombre, apellidos, nickname, password} = req.body;
-
-    const validarQuery = 'SELECT * FROM usuarios WHERE ( email = ?)'
+const crearUsuario = async(req,res)=>{
+    const {correo, nombre, apellidos, nickname, contrasena} = req.body;
+    const validarQuery = 'SELECT * FROM usuarios WHERE ( correo = ?)'
     const insertarQuery = 'INSERT INTO usuarios (nombre, apellidos, correo, contrasena, nickname) Value(?,?,?,?,?)'
-
+    let valores;
     try{
-
-        let query = mysql.format(validarQuery,[email]);
-        
-        await connection.query(validarQuery, (err,result)=>{
-            if (err) throw Error;
-            callback(result)
-        })
-
-        if (query) {
-            return res.status(401).json({
-                ok: false,
-                msg:'Ya existe un usuario con ese correo'
-            })
-        }
-
-        let salt = bcrypt.genSaltSync()
-        
-        query = mysql.format(insertarQuery,[nombre, apellidos, email, bcrypt.hashSync(password,salt), nickname]);
-        await conecction.query(validarQuery,(err,result)=>{
-            if (err) throw Error;
-            callback(result)
+        let queryMysql = mysql.format(validarQuery,[correo]);
+        await conexion.query(queryMysql, (err, results, fields)=>{
+            if (results.length == 0) {
+                queryMysql = null;
+                let salt = bcrypt.genSaltSync()
+                queryMysql = mysql.format(insertarQuery,[nombre, apellidos, correo, bcrypt.hashSync(contrasena,salt), nickname]);
+                conexion.query(queryMysql,(err,results)=>{
+                    if (err) {
+                        res.status(500).json({
+                            ok:false,
+                            error: err,
+                            msg:'Por favor hablar con la administracion'
+                        });
+                    }else{
+                        res.status(400).json({
+                            ok:true,
+                            msg:'los datos han sido ingresados correctamente'
+                        })
+                    }
+                })
+            }else{
+                return res.status(401).json({
+                    ok: false,
+                    msg:'Ya existe un usuario con ese correo'
+                })
+            }
         })  
 
     }catch(e){
-
+        res.status(500).json({
+            ok:false,
+            msg:'Por favor hablar con administracion '
+        })
     }
 
 
     
+}
+
+module.exports ={
+    crearUsuario,
 }

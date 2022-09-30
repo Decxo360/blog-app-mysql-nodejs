f4358f743584bab9db1a8e47a5e22e32a8fd0809
Diego Lundstedt <diegolundstedt99@gmail.com>
2022-09-30 19:40:12 -0300

    metodos para obtener Publicaciones

--- controllers/publicaciones.js
+++ controllers/publicaciones.js
@@ -1,4 +1,67 @@
-const { query } = require('express');
 const express = require('express');
-const dbConexion = require('../db/config');
+const {conexion} = require('../db/config');
 const mysql = require('mysql');
+
+
+const obtenerPublicaciones = async (req,res)=>{
+    const sql = 'SELECT * FROM publicaciones'
+    try{        
+        await conexion.query(sql,(err,results)=>{
+            if (err) {
+                res.status(401).json({
+                    ok:false,
+                    msg:'Ha ocurrido algun error, hablar con administracion'
+                })
+            }else{
+                res.status(400).json({
+                    ok:true,
+                    data:results
+                })
+            }
+        })
+    }catch(e){
+        res.status(500).json({
+            ok:false,
+            msg:'Por favor hablar con administracion'
+        })
+    }
+}
+
+const obtenerPublicacionByTitulo = (req,res)=>{
+
+    const {titulo} = req.body
+    const sql = 'SELECT * FROM publicaciones WHERE(titulo = ?)'
+    const consulta = mysql.format(sql,[titulo])
+
+    try{
+        conexion.query(consulta,(err,results)=>{
+            if (err) {
+                throw new Error(err)
+            }else{
+                if (results.length > 0 ) {
+                    res.status(400).json({
+                        ok:true,
+                        data:results
+                    })
+                }else{
+                    res.status(401).json({
+                        ok:false,
+                        msg:'No existen datos relacionados'
+                    })
+                }
+            }
+        })
+
+    }catch(e){
+        res.status(500).json({
+            ok:false,
+            msg:'Por favor hablar con administracion'
+        })
+    }
+
+}
+
+module.exports={
+    obtenerPublicaciones,
+    obtenerPublicacionByTitulo,
+}

e7d1ad8b0ab262e303f37049a0ffb6f53ac867e6
Diego Lundstedt <diegolundstedt99@gmail.com>
2022-09-29 23:11:42 -0300

    traer todos los usuarios y buscar por nickname

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs');
 const { dbConexion, conexion } = require('../db/config');
 
 
-const crearUsuario = async(req,res)=>{
+const insertUser = async(req,res)=>{
     const {correo, nombre, apellidos, nickname, contrasena} = req.body;
     const validarQuery = 'SELECT * FROM usuarios WHERE ( correo = ?)'
     const insertarQuery = 'INSERT INTO usuarios (nombre, apellidos, correo, contrasena, nickname) Value(?,?,?,?,?)'
@@ -50,6 +50,76 @@ const crearUsuario = async(req,res)=>{
     
 }
 
+
+const searchByNickname = (req,res)=>{
+
+    const {nickname} = req.body;
+
+    const sql = "SELECT * FROM usuarios WHERE (nickname = ?)"
+
+    const query = mysql.format(sql,[nickname]);
+
+    try{
+
+        conexion.query(query,(err,results)=>{
+            if (results.length == 1) {
+                res.status(200).json({
+                    ok:true,
+                    data: results
+                })
+            }else{
+                res.status(401).json({
+                    ok:false,
+                    msg:'los datos no han sido encontrados en la base de datos'
+                })
+            }
+        })
+    }catch(e){
+        res.status(500).json({
+            ok:false,
+            msg:'Por favor comunicarse con la administracion'
+        })
+    }
+
+}
+
+const getUsuarios =(req,res)=>{
+    const sql = 'SELECT * FROM usuarios'
+    try{
+        conexion.query(sql,(err,results)=>{
+            if (err) {
+                throw new Error(err);
+                return;
+            }else{
+                if (results.length>0) {
+                    res.status(200).json({
+                        ok:true,
+                        data:results
+                    })
+                }else{
+                    res.status(500).json({
+                        ok:false,
+                        msg:'No existen datos en la base de datos'
+                    })
+                }
+            }
+        })
+
+    }catch(e){
+        res.status(500).json({
+            ok:false,
+            msg:'Por favor comunicarse con el administrador'
+        })
+    }
+
+}
+
+
+
+
+
 module.exports ={
-    crearUsuario,
+    insertUser,
+    searchByNickname,
+    getUsuarios
 }

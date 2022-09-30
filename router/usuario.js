13cef8be7fa605123c992dd45d0c1c28bdd6c10f
Diego Lundstedt <diegolundstedt99@gmail.com>
2022-09-29 23:12:46 -0300

    nuevas rutas

--- router/usuario.js
+++ router/usuario.js
@@ -1,2 +1,14 @@
 const {Router} = require('express');
+const { insertUser, searchByNickname, getUsuarios } = require('../controllers/usuarios');
+const { dbConexion, conexion } = require('../db/config');
 const router = Router();
+
+
+
+router.post('/crear',insertUser);
+router.get('/buscar',searchByNickname);
+router.get('/all',getUsuarios)
+
+
+
+module.exports = router

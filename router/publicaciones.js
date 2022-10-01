982161d7d016407c259a03581d413a0dac9b30a4
Diego Lundstedt <diegolundstedt99@gmail.com>
2022-09-30 22:58:37 -0300

    nueva ruta

--- router/publicaciones.js
+++ router/publicaciones.js
@@ -1,10 +1,9 @@
 const {Router} = require('express');
-const { obtenerPublicaciones, obtenerPublicacionByTitulo } = require('../controllers/publicaciones');
+const { obtenerPublicaciones, obtenerPublicacionByTitulo, crearPublicacion } = require('../controllers/publicaciones');
 const router = Router();
-
-
 
 router.get('/all', obtenerPublicaciones);
 router.get('/buscarxtitulo', obtenerPublicacionByTitulo);
+router.post('/crear',crearPublicacion);
 
 module.exports = router

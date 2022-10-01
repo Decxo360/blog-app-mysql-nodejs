64a67e787b5be94deff8de30a0711a8fe146c190
Diego Lundstedt <diegolundstedt99@gmail.com>
2022-10-01 19:32:19 -0300

    nuevos metodos para obtener datos

--- controllers/publicaciones.js
+++ controllers/publicaciones.js
@@ -62,13 +62,10 @@ const obtenerPublicacionByTitulo = (req,res)=>{
 }
 
 const crearPublicacion =(req,res)=>{
-
     const {titulo,subtitulo,descripcion,idusuario} = req.body
     const sql = 'INSERT INTO publicaciones (titulo,subtiulo,descripcion,idusuario) Value(?,?,?,?)'
     const consulta = mysql.format(sql,[titulo,subtitulo,descripcion,idusuario]);
-
     try{
-
         conexion.query(consulta,(err,results)=>{
             if (err) {
                 throw new Error(err);
@@ -86,11 +83,93 @@ const crearPublicacion =(req,res)=>{
             msg:'Por favor hablar con administracion'
         })
     }
+}
+
+const eliminarPublicacion =async(req,res)=>{
+    const {idpublicacion} = req.body
+    const sql = 'DELETE FROM publicaciones WHERE(idpublicacion=?)'
+    const consulta = mysql.format(sql,[idpublicacion])
+
+    try{
+
+        await conexion.query(consulta,(err,results)=>{
+            if (err) {
+                res.status(401).json({
+                    ok:false,
+                    msg:'Por favor hablar con la administracion',
+                    err
+                })
+            }else{
+                    console.log(results)
+                if (results.affectedRows > 0) {
+
+                    res.status(200).json({
+                        ok:true,
+                        msg:'Los datos han sido guardados exitosamente'
+                    })
+                }else{
+                    res.status(401).json({
+                        ok:false,
+                        msg:'No existen datos relacionados'
+                    })
+                }
+                
+            }
+        })
+
+    }catch(e){
+        res.status(500).json({
+            ok:false,
+            msg:'Por favor hablar con administracion'
+        })
+    }
+}
+
+const obtenerPublicacionesXIdUsuario = (req,res)=>{
+    const idusuario = req.params.idusuario;
+    const sql = 'SELECT * FROM publicaciones WHERE(idusuario=?)'
+    const consulta = mysql.format(sql,[idusuario])
+
+    try{
+        conexion.query(consulta,(err,results)=>{
+            if(err){
+
+                res.status(401).json({
+                    ok:false,
+                    msg:'Por favor hablar con la administracion',
+                    err
+                })
+
+            }else{
+                if (results.length>0) {
+                    
+                    res.status(200).json({
+                        ok:true,
+                        msg:'Los datos han sido guardados exitosamente',
+                        data:results
+                    })
+
+                }else{
+                    res.status(401).json({
+                        ok:false,
+                        msg:'No existen datos relacionados'
+                    })
+                }
+            }
+        })
+    }catch(e){
+        res.status(500).json({
+            ok:false,
+            msg:'Por favor hablar con administracion'
+        })
+    }
 
 }
 
 module.exports={
     obtenerPublicaciones,
     obtenerPublicacionByTitulo,
-    crearPublicacion
+    crearPublicacion,
+    eliminarPublicacion,
+    obtenerPublicacionesXIdUsuario
 }

02da0c708b110882b2ce35d3413bb604e2b01e27
Decxo360 <diegolundstedt99@gmail.com>
2022-10-05 15:30:54 -0300

    implementacion de la ruta del social login

--- router/auth.js
+++ router/auth.js
@@ -1,8 +1,13 @@
 const {Router} = require('express');
-const { login, insertUser } = require('../controllers/auth');
+const passport = require('passport');
+const { login, insertUser, socialGit } = require('../controllers/auth');
 const router = Router();
 
 router.post('/crear',insertUser);
 router.get('/login',login)
-
+router.get('/social/github', passport.authenticate('github',{scope:['user:email']}))
+router.get('social/github/callback', passport.authenticate('github',{failureRedirect:'/login'}),
+(req,res)=>{
+    res.redirect('/')
+})
 module.exports= router;
